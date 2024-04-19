50477c57d8a2a06d67a500384b20037f5b264deb
CLopMan <cesarlopman@gmail.com>
2024-04-19 16:05:33 +0200

    comentarios

--- src/claves.c
+++ src/claves.c
@@ -22,17 +22,18 @@ int init() {
     char *ip = get_ip();
     if (NULL == ip) return -1;
     printf("ip: %s\n", ip);
+    // crear cliente 
     CLIENT *clnt;
     clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (ip);
+        clnt_pcreateerror (ip); // errores
 		return -1;
     }
     int result;
     enum clnt_stat retval;
     retval = rpc_init_1(&result, clnt);
     if (retval != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
+		clnt_perror (clnt, "call failed"); // errores
 	}
 
     return result;
@@ -41,20 +42,21 @@ int init() {
 int set_value(int key, char* value1, int N_value2, double* V_value2) {
     char *ip = get_ip();
     if (NULL == ip) return -1;
-    CLIENT *clnt;
+    CLIENT *clnt; // crear cliente
     clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (ip);
+        clnt_pcreateerror (ip); // control de errores
 		return -1;
     }
+    // argumentos de entrada
     int result;
     vector_value2 vec;
-    vec.vector_value2_len = (u_int)N_value2;
+    vec.vector_value2_len = (u_int)N_value2; 
     vec.vector_value2_val = V_value2;
     enum clnt_stat retval;
     retval = rpc_set_value_1(key, value1, N_value2, vec, &result, clnt);
     if (retval != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
+		clnt_perror (clnt, "call failed"); // control de errores
 	}
 
     return result;
@@ -64,10 +66,10 @@ int set_value(int key, char* value1, int N_value2, double* V_value2) {
 int get_value(int key, char* value1, int* N_value2, double* V_value2) {
     char *ip = get_ip();
     if (NULL == ip) return -1;
-    CLIENT *clnt;
+    CLIENT *clnt; // crear cliente 
     clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
     if (clnt == NULL) {
-        clnt_pcreateerror (ip);
+        clnt_pcreateerror (ip);  //control de errores
 		return -1; 
     }
     int result;
@@ -75,8 +77,9 @@ int get_value(int key, char* value1, int* N_value2, double* V_value2) {
     
     enum clnt_stat retval = rpc_get_value_1(key, &args, clnt);
     if (retval != RPC_SUCCESS) {
-		clnt_perror (clnt, "call failed");
+		clnt_perror (clnt, "call failed"); //control de errores
 	}
+    // copia de valores
     *N_value2 = args.N_value2;
     strcpy(value1, args.value1);
     memcpy(V_value2, args.value2, *(N_value2 ) * sizeof(double));
@@ -88,12 +91,13 @@ int get_value(int key, char* value1, int* N_value2, double* V_value2) {
 int modify_value(int key, char* value1, int N_value2, double* V_value2) {
     char *ip = get_ip();
     if (NULL == ip) return -1;
-    CLIENT *clnt;
-    clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
+    CLIENT *clnt; // crear cliente
+    clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp"); 
     if (clnt == NULL) {
         clnt_pcreateerror (ip);
 		return -1;
     }
+    // argumentos de entrada
     int result;
     vector_value2 vec;
     vec.vector_value2_val = V_value2;
@@ -110,13 +114,13 @@ int delete_key(int key) {
     char *ip = get_ip();
     if (NULL == ip) return -1;
     CLIENT *clnt;
-    clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
+    clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp"); // crear cliente
     if (clnt == NULL) {
-        clnt_pcreateerror (ip);
+        clnt_pcreateerror (ip); // control de errores
 		return -1;
     }
     int result;
-    enum clnt_stat retval = rpc_delete_key_1(key, &result, clnt);
+    enum clnt_stat retval = rpc_delete_key_1(key, &result, clnt); // control de errores
     if (retval != RPC_SUCCESS) {
 		clnt_perror (clnt, "call failed");
 	}
@@ -125,25 +129,20 @@ int delete_key(int key) {
 }
 
 int exist(int key) {
-    printf("call exist\n");
     char *ip = get_ip();
-    if (NULL == ip) {printf("hola\n"); return -1;}
-    CLIENT *clnt;
+    if (NULL == ip) { return -1;}
+    CLIENT *clnt; // crear cliente
     clnt = clnt_create(ip, CLAVES, CLAVESVER, "tcp");
     if (clnt == NULL) {
-        printf("clng == NULL\n");
-        clnt_pcreateerror (ip);
+        clnt_pcreateerror (ip); // control de errores
 		return -1;
     }
     int result = 1;
-    printf("llamada\n");
     enum clnt_stat retval = rpc_exist_1(key, &result, clnt);
     if (retval != RPC_SUCCESS) {
-        printf("error cliente\n");
-		clnt_perror (clnt, "call failed");
+		clnt_perror (clnt, "call failed"); // control de errores
 	}
 
-    printf("fin\n");
     return result;
 
 }
